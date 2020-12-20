4aab154ecc0805348b6692e919f8cb74ef629745
“MachinistNick” <machinistnarendra@gmail.com>
2020-12-21 03:22:26 +0530

    github project

---
+++ Tuning_NumberOfHiddenNeuron.py
@@ -0,0 +1,76 @@
+# Use scikit-learn to grid search the number of neurons
+import numpy
+from sklearn.model_selection import GridSearchCV
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.wrappers.scikit_learn import KerasClassifier
+from keras.constraints import maxnorm
+
+
+'''
+#GPU disable
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+'''
+
+
+
+
+import os
+os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
+
+
+
+'''
+
+#because of below code GPU start working
+import tensorflow as tf
+#print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
+config = tf.compat.v1.ConfigProto(allow_soft_placement=True)
+
+config.gpu_options.per_process_gpu_memory_fraction = 0.2
+tf.compat.v1.keras.backend.set_session(tf.compat.v1.Session(config=config))
+
+#Above 5 lines of code for GPU 
+
+'''
+
+
+
+
+
+
+# Function to create model, required for KerasClassifier
+def create_model(neurons=1):
+	# create model
+	model = Sequential()
+	model.add(Dense(neurons, input_dim=8, kernel_initializer='uniform', activation='linear', kernel_constraint=maxnorm(4)))
+	model.add(Dropout(0.2))
+	model.add(Dense(1, kernel_initializer='uniform', activation='sigmoid'))
+	# Compile model
+	model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+	return model
+# fix random seed for reproducibility
+seed = 7
+numpy.random.seed(seed)
+# load dataset
+dataset = numpy.loadtxt("pima-indians-diabetes.csv", delimiter=",")
+# split into input (X) and output (Y) variables
+X = dataset[:,0:8]
+Y = dataset[:,8]
+# create model
+model = KerasClassifier(build_fn=create_model, epochs=100, batch_size=10, verbose=0)
+# define the grid search parameters
+neurons = [1, 5, 10, 15, 20, 25, 30]
+param_grid = dict(neurons=neurons)
+grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=-1, cv=3)
+grid_result = grid.fit(X, Y)
+# summarize results
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with: %r" % (mean, stdev, param))
